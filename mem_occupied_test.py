1a36d390c1b609caee1264b19cb32dd618b5d61e
Chanaka Lasantha <129164792+chanakakn@users.noreply.github.com>
2023-06-03 21:25:04 +0530

    Update mem_occupied_test.py

--- mem_occupied_test.py
+++ mem_occupied_test.py
@@ -2,9 +2,12 @@
 import sys
 import os
 import logging
+import openai
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
+
+openai.api_key = 'YOUR_OPENAI_API_KEY'  # Replace with your OpenAI API key
 
 def find_memto_occupy(pc):
     if pc > 100:
@@ -32,6 +35,20 @@ def occupy_memory(mb):
 
     print("Memory released")
 
+def generate_ai_response(prompt):
+    response = openai.Completion.create(
+        engine='text-davinci-003',
+        prompt=prompt,
+        max_tokens=50,
+        n=1,
+        stop=None,
+        temperature=0.6,
+        frequency_penalty=0.0,
+        presence_penalty=0.0
+    )
+
+    return response.choices[0].text.strip()
+
 if __name__ == "__main__":
     try:
         num = sys.argv[1] if len(sys.argv) > 1 else None
@@ -46,6 +63,15 @@ if __name__ == "__main__":
             mb = int(num)
 
         occupy_memory(mb)
+
+        while True:
+            user_input = input("Do you have any questions? (Type 'quit' to exit): ")
+            if user_input.lower() == 'quit':
+                break
+
+            prompt = f"You: {user_input}\nAI:"
+            response = generate_ai_response(prompt)
+            print(response)
 
     except ValueError as ve:
         logger.error(str(ve))
